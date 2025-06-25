7ec6f3e70c37734d288c2f2d42e86a5b87783142
Henrique Pontalti <henrique@logicomp.com.br>
2025-06-25 09:08:15 -0300

    Override subscription check

--- js/pvemanagerlib.js
+++ js/pvemanagerlib.js
@@ -1,1 +1,8 @@
                     me.updateLoginData(data);
+
+// Disable subscription popups
+if (Proxmox.Utils && Proxmox.Utils.checked_command) {
+    Proxmox.Utils.checked_command = function(orig_cmd) {
+        orig_cmd();
+    };
+}
--- touch/pvemanager-mobile.js
+++ touch/pvemanager-mobile.js
@@ -498,36 +498,12 @@ utilities: {
 	);
     },
 
+    // Previously this contacted the subscription API and displayed a
+    // warning when no valid subscription was found. To disable the popup
+    // while keeping the command flow intact, simply execute the callback
+    // directly.
     checked_command: function(orig_cmd) {
-	Proxmox.Utils.API2Request(
-	    {
-		url: '/nodes/localhost/subscription',
-		method: 'GET',
-		failure: function(response, opts) {
-		    Ext.Msg.alert(gettext('Error'), response.htmlStatus);
-		},
-		success: function(response, opts) {
-		    let res = response.result;
-		    if (res === null || res === undefined || !res || res
-			.data.status.toLowerCase() !== 'active') {
-			Ext.Msg.show({
-			    title: gettext('No valid subscription'),
-			    icon: Ext.Msg.WARNING,
-			    message: Proxmox.Utils.getNoSubKeyHtml(res.data.url),
-			    buttons: Ext.Msg.OK,
-			    callback: function(btn) {
-				if (btn !== 'ok') {
-				    return;
-				}
-				orig_cmd();
-			    },
-			});
-		    } else {
-			orig_cmd();
-		    }
-		},
-	    },
-	);
+        orig_cmd();
     },
 
     assemble_field_data: function(values, data) {
